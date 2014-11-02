5e39d5900e589cf4c9b035a3dfde403ca910b698
Fabian Kruse <kruse@web-k.ch>
2014-11-02 19:47:48 +0100

    add secret

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simpsons::Application.config.secret_key_base = 'ad8b3da1d6f7735df04b020086c003480288fb568f85bc699e04047d87e5f9875e779e12262b230023484c28095fca0d2d4cb6041ba64c5d78a42e8b3cad442f'
