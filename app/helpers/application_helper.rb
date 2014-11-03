31043e2f33a4734fe1f42d1625c2d5399123649a
Fabian Kruse <kruse@web-k.ch>
2014-11-03 21:44:54 +0100

    update style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
 		end
 		return "Nein"
 	end
+
+	def self.get_number(season, episode)
+		season = "0" + season.to_s if season < 10
+		episode = "0" + episode.to_s if episode < 10
+
+		return "S" + season.to_s + "E" + episode.to_s
+	end
 end
